129281f870d424d76a01ca375b370e1b7338280a
sudhandeveloper <gmsudhan3@gmail.com>
2023-12-27 23:03:10 +0530

    ne

---
+++ fullstack-indecore/clientside/index.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var cors = require("cors");
+var app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/products", function (req, res, next) {
+  res.json({ msg: "This is CORS-enabled for all origins!" });
+});
+
+app.listen(8000, function () {
+  console.log("CORS-enabled web server listening on port 8000");
+});
---
+++ fullstack-indecore/indeco-decorator/src/dash-board/dashbord-page/dasboard-page.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Dashboardpage = () => {
+  return (
+    <>
+      <div className="m-5">
+        <h2 className="text-center text-[30px] font-RobatoTwo text-red-600">
+          Wellcome to Your DashBoard
+        </h2>
+        <center>    <div className="h-auto p-10 bg-gray-200 w-[25%] mt-5">
+         
+         <img src="" alt="loading" />
+
+       </div> </center>
+    
+      </div>
+    </>
+  );
+};
+
+export default Dashboardpage;
